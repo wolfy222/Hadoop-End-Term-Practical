a42f9e055ffe246b64fdc2c300e1b04cca94bdda
Aadarsh Agarwal <54893794+wolfy222@users.noreply.github.com>
2021-06-17 11:17:22 +0530

    Update and rename MyReducer.java to PrimeReducer.java

--- PrimeReducer.java
+++ PrimeReducer.java
@@ -9,12 +9,16 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class PrimeReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 	public void reduce(Text key, Iterable<IntWritable> values, Context context)
 			throws IOException, InterruptedException {
 		int sum = 0;
 		if (key.equals("PrimeSum")) {
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+		}else{
 			for (IntWritable value : values) {
 				sum += value.get();
 			}
